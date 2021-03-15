403d8711cefa204d8072d46a8c57ff8948a86461
nuwbie-11 <63080447+nuwbie-11@users.noreply.github.com>
2021-03-15 07:22:04 +0700

    Fix Error no module 'wx'
    content :
    - Adding vscode.json to fix 'NoModul'
    - etc

--- Main.py.py
+++ Main.py.py
@@ -1,8 +1,8 @@
 import wx
 import MenghelloWxGui as mywx
 
-
-app = wx.App()
-frame = mywx.MyFrame1(None)
-frame.Show()
-app.MainLoop()
+if __name__ == '__main__':
+    app = wx.App()
+    frame = mywx.MyFrame1(None)
+    frame.Show()
+    app.MainLoop()
